dd28a3b35dece58918a06f567fc437a8f4b37d5e
Nathan Engler <englernathan@gmail.com>
2022-01-20 09:18:18 -0600

    start of endpoints

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 const bodyParser = require("body-parser");
 const db = require("./queries");
 const port = 3000;
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
 app.use(
@@ -16,7 +18,59 @@ app.get("/", function (req, res) {
 });
 
 app.get("/agents", db.getAgents);
-app.get("/maps", db.getMaps);
+
+app.get("/maps", async function (req, res) {
+  const maps = await prisma.map.findMany({});
+  res.send(maps);
+});
+
+app.get("/maps/:id", async function (req, res) {
+  const { id } = req.params;
+
+  const map = await prisma.map.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  const totalPlayers = await prisma.player.count({
+    where: {
+      team: {
+        match: {
+          mapId: map.id,
+        },
+      },
+    },
+  });
+
+  const players = await prisma.player.groupBy({
+    by: ["agentId"],
+    where: {
+      team: {
+        match: {
+          mapId: map.id,
+        },
+      },
+    },
+    _count: true,
+  });
+
+  const agentOptions = {};
+  const agents = await prisma.agent.findMany({});
+  agents.forEach((a) => {
+    agentOptions[a.id] = a.name;
+  });
+
+  const agentPercentages = players.map(function (p) {
+    return {
+      name: agentOptions[p.agentId],
+      totalPicks: p._count,
+      percentage: ((p._count / totalPlayers) * 100).toFixed(2),
+    };
+  });
+
+  res.json({ map: map, agentPercentages: agentPercentages, totalPlayers: totalPlayers });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
--- prisma/seed/matches.js
+++ prisma/seed/matches.js
@@ -8,94 +8,90 @@ async function main() {
   const acts = await prisma.act.findMany();
   const ranksLength = ranks.length;
   const actsLength = acts.length;
+  const iterations = Array(1000).fill(0);
 
   for await (const map of maps) {
-    const randomRankNumber = Math.floor(Math.random() * ranksLength);
-    const startingRank = ranks[randomRankNumber];
-    let randomRanks = [];
-    randomRanks.push(startingRank);
-    if (ranks[randomRankNumber + 1] !== undefined) {
-      randomRanks.push(ranks[randomRankNumber + 1]);
-    }
-    if (ranks[randomRankNumber - 1] !== undefined) {
-      randomRanks.push(ranks[randomRankNumber - 1]);
-    }
+    for await (const _i of iterations) {
+      const randomRankNumber = Math.floor(Math.random() * ranksLength);
+      const startingRank = ranks[randomRankNumber];
+      let randomRanks = [];
+      randomRanks.push(startingRank);
+      if (ranks[randomRankNumber + 1] !== undefined) {
+        randomRanks.push(ranks[randomRankNumber + 1]);
+      }
+      if (ranks[randomRankNumber - 1] !== undefined) {
+        randomRanks.push(ranks[randomRankNumber - 1]);
+      }
 
-    const act = acts[Math.floor(Math.random() * actsLength)];
+      const act = acts[Math.floor(Math.random() * actsLength)];
 
-    const team1Agents = [];
-    const team2Agents = [];
+      const team1Agents = [];
+      const team2Agents = [];
 
-    team1Agents.push(getRandomAgent(agents, team1Agents));
-    team1Agents.push(getRandomAgent(agents, team1Agents));
-    team1Agents.push(getRandomAgent(agents, team1Agents));
-    team1Agents.push(getRandomAgent(agents, team1Agents));
-    team1Agents.push(getRandomAgent(agents, team1Agents));
+      team1Agents.push(getRandomAgent(agents, team1Agents));
+      team1Agents.push(getRandomAgent(agents, team1Agents));
+      team1Agents.push(getRandomAgent(agents, team1Agents));
+      team1Agents.push(getRandomAgent(agents, team1Agents));
+      team1Agents.push(getRandomAgent(agents, team1Agents));
 
-    team2Agents.push(getRandomAgent(agents, team2Agents));
-    team2Agents.push(getRandomAgent(agents, team2Agents));
-    team2Agents.push(getRandomAgent(agents, team2Agents));
-    team2Agents.push(getRandomAgent(agents, team2Agents));
-    team2Agents.push(getRandomAgent(agents, team2Agents));
+      team2Agents.push(getRandomAgent(agents, team2Agents));
+      team2Agents.push(getRandomAgent(agents, team2Agents));
+      team2Agents.push(getRandomAgent(agents, team2Agents));
+      team2Agents.push(getRandomAgent(agents, team2Agents));
+      team2Agents.push(getRandomAgent(agents, team2Agents));
 
-    const match = await prisma.match.create({
-      data: {
-        gameStart: new Date().toISOString(),
-        region: "NA",
-        mapId: map.id,
-        actId: act.id,
-      },
-    });
-
-    const blueTeam = await prisma.team.create({
-      data: {
-        matchId: match.id,
-        didWin: true,
-        teamName: "Blue",
-      },
-    });
-
-    const redTeam = await prisma.team.create({
-      data: {
-        matchId: match.id,
-        didWin: false,
-        teamName: "Red",
-      },
-    });
-
-    for await (const agent of team1Agents) {
-      const agentRandomRank =
-        randomRanks[Math.floor(Math.random() * randomRanks.length)].id;
-      console.log(agentRandomRank);
-      console.log(agent.id);
-      console.log(blueTeam.id);
-      await prisma.player.create({
+      const match = await prisma.match.create({
         data: {
-          rankId: agentRandomRank,
-          agentId: agent.id,
-          teamId: blueTeam.id,
+          gameStart: new Date().toISOString(),
+          region: "NA",
+          mapId: map.id,
+          actId: act.id,
         },
       });
-    }
 
-    for await (const agent of team2Agents) {
-      const agentRandomRank =
-        randomRanks[Math.floor(Math.random() * randomRanks.length)].id;
-      await prisma.player.create({
+      const blueTeam = await prisma.team.create({
         data: {
-          rankId: agentRandomRank,
-          agentId: agent.id,
-          teamId: redTeam.id,
+          matchId: match.id,
+          didWin: true,
+          teamName: "Blue",
         },
       });
+
+      const redTeam = await prisma.team.create({
+        data: {
+          matchId: match.id,
+          didWin: false,
+          teamName: "Red",
+        },
+      });
+
+      for await (const agent of team1Agents) {
+        const agentRandomRank = randomRanks[Math.floor(Math.random() * randomRanks.length)].id;
+        await prisma.player.create({
+          data: {
+            rankId: agentRandomRank,
+            agentId: agent.id,
+            teamId: blueTeam.id,
+          },
+        });
+      }
+
+      for await (const agent of team2Agents) {
+        const agentRandomRank = randomRanks[Math.floor(Math.random() * randomRanks.length)].id;
+        await prisma.player.create({
+          data: {
+            rankId: agentRandomRank,
+            agentId: agent.id,
+            teamId: redTeam.id,
+          },
+        });
+      }
     }
   }
 }
 
 function getRandomAgent(agents, teamAgents) {
-  const remainingAgents = agents.filter(
-    (a) => !teamAgents.map((t) => t.id).includes(a.id)
-  );
+  const remainingAgents = agents.filter((a) => !teamAgents.map((t) => t.id).includes(a.id));
 
   return remainingAgents[Math.floor(Math.random() * remainingAgents.length)];
 }
