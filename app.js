1a1a8fbe13aa1459dad125116e04e02028d44d4c
Nathan Engler <englernathan@gmail.com>
2022-01-21 16:09:27 -0600

    improved maps/:id endpoint and added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+var cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./queries");
 const port = 3000;
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -25,11 +27,11 @@ app.get("/maps", async function (req, res) {
 });
 
 app.get("/maps/:id", async function (req, res) {
-  const { id } = req.params;
+  const id = parseInt(req.params.id);
 
   const map = await prisma.map.findUnique({
     where: {
-      id: parseInt(id),
+      id: id,
     },
   });
 
@@ -69,7 +71,15 @@ app.get("/maps/:id", async function (req, res) {
     };
   });
 
-  res.json({ map: map, agentPercentages: agentPercentages, totalPlayers: totalPlayers });
+  const mostCommonComp =
+    await prisma.$queryRaw`WITH agentAgg as (select array_agg("Agent"."name" order by "Agent"."name") as agents FROM "Team" INNER JOIN "Match" on "Match".id="Team"."matchId" INNER JOIN "Player" ON "Player"."teamId"="Team".id INNER JOIN "Agent" on "Agent".id="Player"."agentId" where "Match"."mapId"=${map.id} group by "Team".id) select count (agents), agents from agentAgg group by agentAgg.agents order by count(agents) DESC LIMIT 5;`;
+
+  res.json({
+    map: map,
+    agentPercentages: agentPercentages,
+    totalPlayers: totalPlayers,
+    mostCommonComp: mostCommonComp,
+  });
 });
 
 app.listen(port, () => {
